529bb5a56d752d307485c1b93fac99580a8d096a
Valdeilson-Lima <valdeilsonbdl56@gmail.com>
2025-03-06 00:34:09 -0300

    Concluido função adicionar no carrinho, valdações de campos e envio de pedido pelo Whatsapp

--- script.js
+++ script.js
@@ -5,14 +5,17 @@ const cartItemsContainer = document.getElementById("cart-items")
 const cartTotal = document.getElementById("cart-total")
 const checkoutBtn = document.getElementById("checkout-btn")
 const closeModalBtn = document.getElementById("close-modal-btn")
-const addressinput = document.getElementById("address")
+const addressInput = document.getElementById("address")
 const addressWarn = document.getElementById("address-warn")
+const cartCounter = document.getElementById("cart-count")
 
 let cart = [];
 
 // Abrir o modal do carrinho
 cartBtn.addEventListener("click", function(){
+    updateCartModal()
     cartModal.style.display = "flex"
+    
 })
 
 //Fechar o modal quando clicar fora
@@ -47,14 +50,155 @@ function addToCart(name, price){
 
     if (existingItem){
         existingItem.quantity += 1
-        return
+       
+    } else{
+
+        cart.push({
+            name, 
+            price,
+            quantity: 1,
+        })
+    }
+
+
+    updateCartModal()
+}
+
+function updateCartModal(){
+    cartItemsContainer.innerHTML = "";
+    let total = 0;
+
+    cart.forEach(item => {
+        const cartItemElement = document.createElement("div")
+        cartItemElement.classList.add("flex", "justify-between", "mb-4", "flex-col")
+
+
+        cartItemElement.innerHTML = `
+        <div class="flex items-center justify-between">
+            <div>
+                <p class="font-bold">${item.name}</p>
+                <p>Qtd: ${item.quantity}</p>
+                <p class="font-medium mt-2">R$ ${item.price.toFixed(2)}</p>
+            </div>
+
+            
+                <button class="remove-btn cursor-pointer" data-name="${item.name}">
+                    Remover
+                </button>
+            
+        </div>
+        
+        `
+
+        total += item.price * item.quantity
+        cartItemsContainer.appendChild(cartItemElement)
+    })
+
+    cartTotal.textContent =total.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    });
+
+    cartCounter.innerHTML = cart.length;
+}
+
+//Função para remover o item do carrinho
+
+cartItemsContainer.addEventListener("click", function(event){
+    if(event.target.classList.contains("remove-btn")){
+        const name = event.target.getAttribute("data-name")
+        removeItemCart(name)
+       
+    }
+})
+
+function removeItemCart(name){
+    const index = cart.findIndex(item => item.name === name);
+
+    if (index !== -1){
+        const item = cart[index];
+        if (item.quantity > 1){
+            item.quantity -= 1
+            updateCartModal();
+            return
+        }
+
+        cart.splice(index, 1);
+        updateCartModal();
+    }
+}
+
+addressInput.addEventListener("input", function(event){
+    let inputValue = event.target.value;
+
+    if (inputValue !== ""){
+        addressInput.classList.remove("border-red-500")
+        addressWarn.classList.add("hidden")
+    }
+
+})
+
+// Finalizar pedido
+checkoutBtn.addEventListener("click", function(){
+
+    const isOpen = checkRestaurantOpen();
+    if (!isOpen){
+        
+        Toastify(
+            {
+            text: "Ops, o restaurante está fechado!",
+            duration: 3000,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+            background: "#ef4444",
+            },
+            }).showToast();
+        return;
     }
 
     
 
-    cart.push({
-        name, 
-        price,
-        quantity: 1,
-    })
+    if (cart.length === 0) return
+    if(addressInput.value === ""){
+        addressWarn.classList.remove("hidden")
+        addressInput.classList.add("border-red-500")
+        return
+    }
+
+    // Enviar o pedido para Whtsapp
+    const cartItems = cart.map((item) => {
+        return(
+            `${item.name} Quantidade: (${item.quantity}) Preço: R$${item.price} | `
+        )
+    }).join("")
+    
+    const message = encodeURIComponent(cartItems)
+    const phone ="83993385224"
+
+    window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`, "_blank")
+
+    cart.length = 0;
+    updateCartModal()
+
+    
+})
+
+function checkRestaurantOpen(){
+    const data = new Date();
+    const hora = data.getHours();
+    return hora >= 18 && hora < 22;
+}
+
+const spanItem = document.getElementById("date-span")
+const isOpen = checkRestaurantOpen();
+
+if (isOpen){
+    spanItem.classList.remove("bg-red-500")
+    spanItem.classList.add("bg-green-600")
+}else{
+    spanItem.classList.remove("bg-green-600")
+    spanItem.classList.add("bg-red-500")
 }
